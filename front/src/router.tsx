68ec51f2b7f3ffa72e4e5fa5f3c89bb409924582
gbnijnugyf <1612517397@qq.com>
2023-07-07 17:16:09 +0800

    add router index by navigate

--- front/src/router.tsx
+++ front/src/router.tsx
@@ -1,15 +1,25 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { MainPage } from "./pages/mainPage";
 import { DisplayPage } from "./pages/displayPage";
 
 export function Routers() {
   return (
     <Routes>
-      <Route path="" element={<MainPage />}>
-        <Route index path="display" element={<DisplayPage/>} />
+      <Route path="/*" element={<MainPage />}>
+        <Route path="display" element={<DisplayPage />} />
         <Route path="upload" element={<></>} />
         <Route path="else" element={<></>} />
       </Route>
+
+      <Route
+        index
+        element={
+          <Navigate
+            to={"display"}
+            replace
+          />
+        }
+      />
     </Routes>
   );
 }
